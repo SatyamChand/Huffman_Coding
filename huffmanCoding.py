b6e018df94fbd4dce10a6024fd80227fb355e4d0
satyamchand <satyamdeopa@gmail.com>
2020-12-24 23:03:31 +0530

    Ease of readability

--- huffmanCoding.py
+++ huffmanCoding.py
@@ -1,4 +1,4 @@
-class Node:   
+class Node:                                                        # A node for huffman tree
     
     def __init__(self,freq,data=None):
         self.data=data
@@ -23,7 +23,11 @@ class Node:
             
     
     
+
+
+
 class Huffman:
+
     def __init__(self):
         self.text=''
         self.codes={}
@@ -34,30 +38,53 @@ class Huffman:
         self.header=''
         self.completebytestr=''
     
-    def encode(self):
-        
-        return
-        
-    def showCodes(self):
+
+
+
+    def showCodes(self):                                    #to help user identify codes for different symbols
         for k in self.codes:
             print(k,':',self.codes[k])
-    
+
+
+
+
+    def encodeFromFile(self,filename):                      #reading text from a file
+        content=''
+        self.encode(content)
+
+
+
+
+
+    def encode(self,text):                                  #driver to encode text
+        self.textInput(text)
+        self.createHuffmanTree()
+        #self.showCodes()
+        entext=self.encodeText()
+        self.padText()
+        self.createHeader()
+        self.completion()
+
+
+
+
     def textInput(self,inText):
         self.text=inText
-        print(inText)
+
         freq_count={}
-        self.tree=[]
-        for ch in self.text:
+        for ch in self.text:                                #count frequency of symbols
             if ch not in freq_count:
                 freq_count[ch]=0
             freq_count[ch]+=1
+
+        self.tree=[]                                        #creation of nodes to make Huffman tree
         for key in sorted(freq_count,key=freq_count.get):
             self.tree.append(Node(freq_count[key],key))
-        print('text=',self.text)
         
-        
-        
-    def createHuffmanTree(self):
+
+
+
+    def createHuffmanTree(self):                            #Creation of tree
         while(len(self.tree)>1):
             new_node = Node(self.tree[0].freq+self.tree[1].freq)
             new_node.createChild(self.tree[0],self.tree[1])
@@ -66,131 +93,142 @@ class Huffman:
             if(len(self.tree)==0):
                 self.tree=new_node
                 break
-            sm=False
+            notEndNode=False
             for i in range(len(self.tree)):
                 if self.tree[i].freq>new_node.freq:
-                    sm=True
+                    notEndNode=True
                     break
-                    
-            if sm:
+            if notEndNode:
                 self.tree.insert(i,new_node)
             else:
                 self.tree.append(new_node)
         
         self.createCodes(self.tree)
         
-    
-    def createCodes(self,node,string=''):
+
+
+
+    def createCodes(self,node,string=''):                      #returns code for a text symbol
         if node.data:
             self.codes[string]=node.data
             return
         self.createCodes(node.left,string+'0')
         self.createCodes(node.right,string+'1')
         
-            
-    def encodeText(self):
+
+
+
+    def encodeText(self):                                       #encodes text using dictionary codes, returns string
         e_code=''
-        encoding_codes={}
+        
+        encoding_codes={}                                       #creating reverse dict since self.codes stores codes for decoding
         for key in self.codes:
             encoding_codes[self.codes[key]]=key
-        print(encoding_codes)
+        
         for ch in self.text:
             e_code+=encoding_codes[ch]
-        print(self.text)
-        print(e_code)
+
         self.coded_text=e_code
-        print('coded text :',self.coded_text)
-        #print(len(self.coded_text))
         return self.coded_text
     
-    def padText(self):
+
+
+
+    def padText(self):                                          #to pad the text in counts of 8 since we need to create binary string and a byte can only hold 8 binaries
         self.padding=len(self.coded_text)%8
         if self.padding!=0:
             self.padding=8-self.padding
             self.coded_text+='0'*self.padding
-        #print(len(self.coded_text),self.coded_text)
 
-        coded_str=bytearray()
-        for i in range(0,len(self.coded_text),8):
+        coded_str=bytearray()                                   
+        for i in range(0,len(self.coded_text),8):               #conversion to bytes
             coded_str.append(int(self.coded_text[i:i+8],2))
-        print(coded_str)
         self.coded_bytestr=coded_str
         return coded_str
     
-    def createHeader(self):
+
+
+
+    def createHeader(self):                                     #returns header that stores relevent data for decoding the file (in byte form)
         header=bytearray()
-        #print(self.padding)
-        #print(self.coded_text)
-        #print(len(self.coded_text))
         header.append(self.padding)
         header.append(len(self.codes))
         for code in self.codes:
-            #byte=int(code,2)
             byte=map(ord,code)
-            #print(*byte)
             for x in byte:
                 header.append(x)
             header.append(127)
-            #print(ord(self.codes[code]))
             byte=int(ord(self.codes[code]))
             header.append(byte)
         self.header=header
-        #print(header)
         
             
-    def completion(self):
+
+
+    def completion(self):                                       
         b=self.header+self.coded_bytestr
-        #print(b)
-        self.completebytestr=b
+        self.completebytestr=b                                  #content ready to be written
         
+    def writeToFile(self,filename):                             #to write a binary file with specified name
+        with open(filename,'wb') as file:
+            file.write(self.completebytestr)
+        print('Content written to file : ',self.text)
+    '''    
     def writeToFile(self,file):
         file.write(self.completebytestr)
         print('Content written')
-        
+    '''
 
-    #----------------------------------------
+
+    #----------------------------------------decode section-----------------------------------------
     
-    def readFromFile(self,file):
-        with open('text.text','rb') as file:
-            byte=file.read()
-            #print(byte)
-            #print(int(byte[1]))
-            #print(byte[2:])
-            self.completebytestr=byte
+
+    def decodeFromFile(self,filename):                          #read content for decoding
+        with open(filename,'rb') as file:
+            content=file.read()
+            self.decode(content)
+
+
+
     
-    def splitContents(self):
-        print(self.completebytestr)
+    def decode(self,content):                                   #driver to decode content
+        self.completebytestr=content
+        self.splitContents()
+        self.decodeOutput()
+
+
+
+
+    def splitContents(self):                                    #splits data to relevent components
         self.padding=int(self.completebytestr[0])
         count=int(self.completebytestr[1])
+
         i=0
         j=2
         codes={}
-        while i<count:
+        while i<count:                                          #creation of dictionary for decoding
             code=''
-            #print(chr(self.completebytestr[j]))
-            while self.completebytestr[j] != 127:
-                #print('.',end=' ')
+            while self.completebytestr[j] != 127:               #value 127 represents end of string here, used here to let te program know the code sting for a symbol has ended
                 code+=chr(self.completebytestr[j])
                 j+=1
-            #print(code)
             codes[code]=chr(self.completebytestr[j+1])
             j+=2
             i+=1
-        print(codes)
+
+        
         barray=bytearray(self.completebytestr[j:])
-        print(barray)
         b=''
         barray=map(bin,barray)
         
-        #some decoding error here---------------------------------------------
-        #fixed
         for x in barray:
-            b+=x[2:].rjust(8,'0')
-        print(b)
+            b+=x[2:].rjust(8,'0')                               #conversion to a regular string for comprehesion and removing the padding
+        self.codes=codes
         self.coded_text=b[:-self.padding]
-    
-    def decode(self):
-        print(self.coded_text)
+
+
+
+
+    def decodeOutput(self):                                     #decode the string
         decodedtext=''
         s=''
         for ch in self.coded_text:
@@ -198,6 +236,6 @@ class Huffman:
             if s in self.codes:
                 decodedtext+=self.codes[s]
                 s=''
-        print(decodedtext)
+        print(decodedtext,'\n\n\n')
             
 
--- main.py
+++ main.py
@@ -6,22 +6,12 @@ text="I am here!!"
 
 
 huff = huffmanCoding.Huffman()
-huff.textInput(text)
-huff.createHuffmanTree()
-#huff.showCodes()
-entext=huff.encodeText()
-huff.padText()
-huff.createHeader()
-huff.completion()
+huff.encode(text)
 
-
-
-with open('text.text','wb') as file:
-    huff.writeToFile(file)
+filename='demo.bin'
+huff.writeToFile(filename)
 
 print("\n\nNow decoding-------------------------------------------------------------------\n")
 
-with open('text.text','rb') as file:
-    huff.readFromFile(file)
-huff.splitContents()
-huff.decode()
+huff.decodeFromFile(filename)
+
